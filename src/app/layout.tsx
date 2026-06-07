48db5b0262942dc75fe350c968fdba29e92aa7f1
Yelihi <yelihi19@gmail.com>
2026-06-07 13:36:35 +0900

    chore: font noto sans kr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
+
+// font
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "./providers/QueryProvider";
+
+const notoSansKR = Noto_Sans_KR({
+  subsets: ["latin"],
+  variable: "--font-noto-sans-kr",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "english-speaking-practice",
@@ -9,7 +18,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={`${notoSansKR.variable}`}>
       <body>
         <QueryProvider>{children}</QueryProvider>
       </body>
