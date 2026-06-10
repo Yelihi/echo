eff94168515b2f01c6390c1c4aa5545f72b2f70d
Yelihi <Yelihi19@gmail.com>
2026-06-10 10:53:02 +0900

    feat: login layout setting

---
+++ src/app/(auth)/login/layout.tsx
@@ -0,0 +1,7 @@
+function LoginLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="flex w-full h-lvh justify-center items-center bg-white">{children}</section>
+  );
+}
+
+export default LoginLayout;
